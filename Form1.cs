bb030caa3bce13ccd289d801b8f19f0406523427
Rodriguez Garcia Juan Pablo <a343900@alumnos.uaslp.mx>
2023-09-11 12:27:34 -0600

    Ya funciona, agregue un archivo para probarlo. Solo faltan los iconos

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,9 @@ namespace Proyecto_U1_3_Arbol
 {
     public partial class Form1 : Form
     {
-        string[] txt;
+        //Variables para leer el archivo y organizar el arbol
+        string[] txt; //Para guardar el archivo
+        int i, j, k, lin; //Como indices
         public Form1()
         {
             InitializeComponent();
@@ -21,29 +23,39 @@ namespace Proyecto_U1_3_Arbol
 
         private void botonAbrir_Click(object sender, EventArgs e)
         {
+            //Limpieza de arbol
+            i = j = k = lin = 0;
             arbolDatos.Nodes.Clear();
-            OpenFileDialog openFileDialog = new OpenFileDialog();
 
+            //Usuario da un archivo
+            OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Archivo de texto|*.txt";
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            if (openFileDialog.ShowDialog() == DialogResult.OK) //Si lee algo
             {
-                txt = File.ReadAllLines(openFileDialog.FileName);
-
-                for(int i = 0; i < txt.Length; i++)
+                txt = File.ReadAllLines(openFileDialog.FileName); //Guarda el archivo por lineas
+                
+                while (lin < txt.Length) //Empieza a acomodar el arbol
                 {
-                    if (txt[i][0] == '.')
+                    MessageBox.Show(txt[lin]);
+                    if (txt[lin][0] == '.' && txt[lin][1] == '.') //Si es nieto
                     {
-                        arbolDatos.Nodes[i - 1].Nodes.Add(txt[i].Substring(1));
+                        arbolDatos.Nodes[i - 1].Nodes[j - 1].Nodes.Add(txt[lin].Substring(2));
+                        k++;
                     }
-                    else if (txt[i][0] == '.' && txt[i][1] == '.')
+                    else if (txt[lin][0] == '.') //Si es hijo
                     {
-                        arbolDatos.Nodes[i - 1].Nodes[i - 1].Nodes.Add(txt[i].Substring(2));
+                        arbolDatos.Nodes[i - 1].Nodes.Add(txt[lin].Substring(1));
+                        j++;
+                        k = 0;
                     }
-                    else
+                    else //Si es padre
                     {
-                        arbolDatos.Nodes.Add(txt[i]);
+                        arbolDatos.Nodes.Add(txt[lin]);
+                        i++;
+                        j = k = 0;
                     }
+                    lin++;
                 }
             }
 
