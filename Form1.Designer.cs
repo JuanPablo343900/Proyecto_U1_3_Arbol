6672df66241df6a7baf6fb461c17f11e7c8803bb
Rodriguez Garcia Juan Pablo <a343900@alumnos.uaslp.mx>
2023-09-11 09:18:23 -0600

    El arbol ya lee los archivos, falta hacer: Mas pruebas Agregar imagenes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.arbolDatos = new System.Windows.Forms.TreeView();
+            this.botonAbrir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // arbolDatos
+            // 
+            this.arbolDatos.Location = new System.Drawing.Point(12, 12);
+            this.arbolDatos.Name = "arbolDatos";
+            this.arbolDatos.Size = new System.Drawing.Size(212, 426);
+            this.arbolDatos.TabIndex = 0;
+            // 
+            // botonAbrir
+            // 
+            this.botonAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAbrir.Location = new System.Drawing.Point(256, 12);
+            this.botonAbrir.Name = "botonAbrir";
+            this.botonAbrir.Size = new System.Drawing.Size(96, 40);
+            this.botonAbrir.TabIndex = 1;
+            this.botonAbrir.Text = "Abrir";
+            this.botonAbrir.UseVisualStyleBackColor = true;
+            this.botonAbrir.Click += new System.EventHandler(this.botonAbrir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botonAbrir);
+            this.Controls.Add(this.arbolDatos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView arbolDatos;
+        private System.Windows.Forms.Button botonAbrir;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Proyecto_U1_3_Arbol
 {
     public partial class Form1 : Form
     {
+        string[] txt;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void botonAbrir_Click(object sender, EventArgs e)
+        {
+            arbolDatos.Nodes.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Archivo de texto|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txt = File.ReadAllLines(openFileDialog.FileName);
+
+                for(int i = 0; i < txt.Length; i++)
+                {
+                    if (txt[i][0] == '.')
+                    {
+                        arbolDatos.Nodes[i - 1].Nodes.Add(txt[i].Substring(1));
+                    }
+                    else if (txt[i][0] == '.' && txt[i][1] == '.')
+                    {
+                        arbolDatos.Nodes[i - 1].Nodes[i - 1].Nodes.Add(txt[i].Substring(2));
+                    }
+                    else
+                    {
+                        arbolDatos.Nodes.Add(txt[i]);
+                    }
+                }
+            }
+
         }
     }
 }
