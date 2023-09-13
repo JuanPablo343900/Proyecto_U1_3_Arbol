8471e23f8177b958bb260dcad909bc87bf658905
Rodriguez Garcia Juan Pablo <a343900@alumnos.uaslp.mx>
2023-09-13 14:34:19 -0600

    Segun yo, ya esta completo

--- Form1.cs
+++ Form1.cs
@@ -15,16 +15,22 @@ namespace Proyecto_U1_3_Arbol
     {
         //Variables para leer el archivo y organizar el arbol
         string[] txt; //Para guardar el archivo
-        int i, j, k, lin; //Como indices
+        int i, j, k, l, lin; //Como indices
         public Form1()
         {
             InitializeComponent();
+            arbolDatos.ImageList = new ImageList();
+            arbolDatos.ImageList.Images.Add(Image.FromFile("C:/Users/JPRZ1/source/repos/Cid-Lufaine/Proyecto_U1_3_Arbol/circle-icon.png"));
+            arbolDatos.ImageList.Images.Add(Image.FromFile("C:/Users/JPRZ1/source/repos/Cid-Lufaine/Proyecto_U1_3_Arbol/circle-line-icon.png"));
+            arbolDatos.ImageList.Images.Add(Image.FromFile("C:/Users/JPRZ1/source/repos/Cid-Lufaine/Proyecto_U1_3_Arbol/circle-center-icon.png"));
+            arbolDatos.ImageList.Images.Add(Image.FromFile("C:/Users/JPRZ1/source/repos/Cid-Lufaine/Proyecto_U1_3_Arbol/circle-center-line-icon.png"));
+            arbolDatos.ImageList.Images.Add(Image.FromFile("C:/Users/JPRZ1/source/repos/Cid-Lufaine/Proyecto_U1_3_Arbol/color-adjust-icon.png"));
         }
 
         private void botonAbrir_Click(object sender, EventArgs e)
         {
             //Limpieza de arbol
-            i = j = k = lin = 0;
+            i = j = k = l = lin = 0;
             arbolDatos.Nodes.Clear();
 
             //Usuario da un archivo
@@ -34,26 +40,35 @@ namespace Proyecto_U1_3_Arbol
             if (openFileDialog.ShowDialog() == DialogResult.OK) //Si lee algo
             {
                 txt = File.ReadAllLines(openFileDialog.FileName); //Guarda el archivo por lineas
-                
+
                 while (lin < txt.Length) //Empieza a acomodar el arbol
                 {
-                    MessageBox.Show(txt[lin]);
-                    if (txt[lin][0] == '.' && txt[lin][1] == '.') //Si es nieto
+                    if (txt[lin][0] == '.' && txt[lin][1] == '.' && txt[lin][2] == '.') //Si es bisnieto
+                    {
+                        arbolDatos.Nodes[i - 1].Nodes[j - 1].Nodes[k - 1].Nodes.Add(txt[lin].Substring(3));
+                        arbolDatos.Nodes[i - 1].Nodes[j - 1].Nodes[k - 1].Nodes[l].ImageIndex = 4;
+                        l++;
+                    }
+                    else if (txt[lin][0] == '.' && txt[lin][1] == '.') //Si es nieto
                     {
                         arbolDatos.Nodes[i - 1].Nodes[j - 1].Nodes.Add(txt[lin].Substring(2));
+                        arbolDatos.Nodes[i - 1].Nodes[j - 1].Nodes[k].ImageIndex = 3;
                         k++;
+                        l = 0;
                     }
                     else if (txt[lin][0] == '.') //Si es hijo
                     {
                         arbolDatos.Nodes[i - 1].Nodes.Add(txt[lin].Substring(1));
+                        arbolDatos.Nodes[i - 1].Nodes[j].ImageIndex = 2;
                         j++;
-                        k = 0;
+                        k = l = 0;
                     }
                     else //Si es padre
                     {
                         arbolDatos.Nodes.Add(txt[lin]);
+                        arbolDatos.Nodes[i].ImageIndex = 1;
                         i++;
-                        j = k = 0;
+                        j = k = l = 0;
                     }
                     lin++;
                 }
